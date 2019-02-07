4967c77e18a95476a56817424a138ebf0ccf12e1
troturier <thibaultroturier@gmail.com>
2019-02-07 18:08:48 +0100

    Ajout de ViewModelFactory

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/injections/ViewModelFactory.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.realestatemanager.injections;
+
+import com.openclassrooms.realestatemanager.repositories.BienImmobilierDataRepository;
+import com.openclassrooms.realestatemanager.repositories.PhotoDataRepository;
+import com.openclassrooms.realestatemanager.repositories.PointInteretDataRepository;
+import com.openclassrooms.realestatemanager.repositories.TypeDataRepository;
+import com.openclassrooms.realestatemanager.repositories.UtilisateurDataRepository;
+import com.openclassrooms.realestatemanager.viewmodels.BienImmobilierViewModel;
+
+import java.util.concurrent.Executor;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+    private final BienImmobilierDataRepository bienImmobilierDataSource;
+    private final PhotoDataRepository photoDataSource;
+    private final PointInteretDataRepository pointInteretDataSource;
+    private final TypeDataRepository typeDataSource;
+    private final UtilisateurDataRepository utilisateurDataSource;
+    private final Executor executor;
+
+    public ViewModelFactory(BienImmobilierDataRepository bienImmobilierDataSource, PhotoDataRepository photoDataSource, PointInteretDataRepository pointInteretDataSource, TypeDataRepository typeDataSource, UtilisateurDataRepository utilisateurDataSource, Executor executor){
+        this.bienImmobilierDataSource = bienImmobilierDataSource;
+        this.photoDataSource = photoDataSource;
+        this.pointInteretDataSource = pointInteretDataSource;
+        this.typeDataSource = typeDataSource;
+        this.utilisateurDataSource = utilisateurDataSource;
+        this.executor = executor;
+    }
+
+    @Override
+
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(BienImmobilierViewModel.class)) {
+            return (T) new BienImmobilierViewModel(bienImmobilierDataSource, photoDataSource, pointInteretDataSource, typeDataSource, utilisateurDataSource, executor);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class");
+    }
+}
