7b0fd379e616e4840722f00db55f7f469c697fba
troturier <thibaultroturier@gmail.com>
2019-02-17 19:19:29 +0100

    Ajout des icônes Ajout/Edition/Recherche à l'action bar de l'application

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/MainActivity.java
@@ -1,19 +1,24 @@
 package com.openclassrooms.realestatemanager.controllers.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.controllers.fragments.RealEstateListFragment;
+import com.openclassrooms.realestatemanager.models.BienImmobilierComplete;
 import com.openclassrooms.realestatemanager.repositories.injections.Injection;
 import com.openclassrooms.realestatemanager.repositories.injections.ViewModelFactory;
-import com.openclassrooms.realestatemanager.models.BienImmobilier;
-import com.openclassrooms.realestatemanager.models.BienImmobilierComplete;
 import com.openclassrooms.realestatemanager.utils.Utils;
 import com.openclassrooms.realestatemanager.viewmodels.BienImmobilierViewModel;
 
 import java.util.List;
+import java.util.Objects;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -21,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
     // 1 - FOR DATA
     private BienImmobilierViewModel bienImmobilierViewModel;
-    private List<BienImmobilierComplete> bienImmobilierList;
-    private static int USER_ID = 1;
 
     public boolean isTwoPane = false;
 
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         determinePaneLayout();
         if(!Utils.checkPermissionForReadExtertalStorage(this)){
             try {
@@ -52,14 +56,17 @@ public class MainActivity extends AppCompatActivity {
     private void configureViewModel(){
         ViewModelFactory mViewModelFactory = Injection.provideViewModelFactory(this);
         this.bienImmobilierViewModel = ViewModelProviders.of(this, mViewModelFactory).get(BienImmobilierViewModel.class);
+        int USER_ID = 1;
         this.bienImmobilierViewModel.init(USER_ID);
     }
 
-    // 3 - Get Current Utilisateur
-    private void getCurrentUtilisateur(int utilisateurId){
-        this.bienImmobilierViewModel.getUtilisateur(utilisateurId);
-        // this.bienImmobilierViewModel.getUtilisateur(utilisateurId).observe(this, this::updateHeader);
-    }
+    // --Commented out by Inspection START (17/02/2019 18:47):
+    //    // 3 - Get Current Utilisateur
+    //    private void getCurrentUtilisateur(int utilisateurId){
+    //        this.bienImmobilierViewModel.getUtilisateur(utilisateurId);
+    //        // this.bienImmobilierViewModel.getUtilisateur(utilisateurId).observe(this, this::updateHeader);
+    //    }
+    // --Commented out by Inspection STOP (17/02/2019 18:47)
 
     // 3 - Get all BienImmobiliers
     private void getBienImmobiliers(){
@@ -67,20 +74,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // 3 - Create a new BienImmobilier
-    /**private void createBienImmobilier(){
-     BienImmobilier bienImmobilier = new BienImmobilier(//FORM FIELDS DATA);
-     this.bienImmobilierViewModel.createBienImmobilier(bienImmobilier);
-     }*/
+    // --Commented out by Inspection START (17/02/2019 18:47):
+    //    /**private void createBienImmobilier(){
+    //     BienImmobilier bienImmobilier = new BienImmobilier(//FORM FIELDS DATA);
+    //     this.bienImmobilierViewModel.createBienImmobilier(bienImmobilier);
+    // --Commented out by Inspection START (17/02/2019 18:47):
+    ////     }*/
+    ////
+    ////    // 3 - Delete a bienImmobilier
+    ////    private void deleteBienImmobilier(BienImmobilier bienImmobilier){
+    ////        this.bienImmobilierViewModel.deleteBienImmobilier(bienImmobilier);
+    // --Commented out by Inspection STOP (17/02/2019 18:47)
+    //    }
+    // --Commented out by Inspection STOP (17/02/2019 18:47)
 
-    // 3 - Delete a bienImmobilier
-    private void deleteBienImmobilier(BienImmobilier bienImmobilier){
-        this.bienImmobilierViewModel.deleteBienImmobilier(bienImmobilier);
-    }
-
-    // 3 - Update a bienImmobilier
-    private void updateBienImmobilier(BienImmobilier bienImmobilier){
-        this.bienImmobilierViewModel.updateBienImmobilier(bienImmobilier);
-    }
+    // --Commented out by Inspection START (17/02/2019 18:47):
+    //    // 3 - Update a bienImmobilier
+    //    private void updateBienImmobilier(BienImmobilier bienImmobilier){
+    //        this.bienImmobilierViewModel.updateBienImmobilier(bienImmobilier);
+    //    }
+    // --Commented out by Inspection STOP (17/02/2019 18:47)
 
     // -------------------
     // UI
@@ -93,9 +106,8 @@ public class MainActivity extends AppCompatActivity {
 
     // 6 - Update the list of items
     private void updateBienImmobiliersList(List<BienImmobilierComplete> bienImmobilierList){
-        this.bienImmobilierList = bienImmobilierList;
         RealEstateListFragment realEstateListFragment = (RealEstateListFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentRealEstateList);
-        realEstateListFragment.updateAdapter(this.bienImmobilierList);
+        Objects.requireNonNull(realEstateListFragment).updateAdapter(bienImmobilierList);
     }
 
     private void configure(){
@@ -106,14 +118,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void determinePaneLayout() {
-        FrameLayout fragmentDetail = (FrameLayout) findViewById(R.id.flDetailContainer);
+        FrameLayout fragmentDetail = findViewById(R.id.flDetailContainer);
         if(fragmentDetail != null){
             isTwoPane = true;
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Toast.makeText(this, "Add button selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_edit:
+                Toast.makeText(this, "Edit button selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_search:
+                Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
         configure();
     }
 }
